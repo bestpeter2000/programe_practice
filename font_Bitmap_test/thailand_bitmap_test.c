f410ca3ea1c354ad842db3c46e06d44293b47db2
peter <you@example.com>
2022-09-30 17:51:34 +0800

    TO DO bitmap shift action

--- font_Bitmap_test/thailand_bitmap_test.c
+++ font_Bitmap_test/thailand_bitmap_test.c
@@ -103,14 +103,14 @@ char* get_data_from_resource(int type,char *bitmap ,short unicode_addr)
 		font_addr_start = 0x0000503E;
 		font_size = 76;
 		font_width = 3;
-		printf("You select M font size, font start address is 0x%.04lX\n\n", font_addr_start);        
+		printf("You select M font size, font start address is 0x%.04X\n\n", font_addr_start);        
     }
     else if(type==1)
     {
 		font_addr_start = 0x004C513E;
 		font_size = 137;
 		font_width = 4;
-		printf("You select L font size, font start address is 0x%.04lX\n\n", font_addr_start);        
+		printf("You select L font size, font start address is 0x%.04X\n\n", font_addr_start);        
     }
     else
 	{
@@ -168,41 +168,48 @@ char* get_data_from_resource(int type,char *bitmap ,short unicode_addr)
     
 }
 
-void combine_bitmap(char *dst_bitmap , char *src_bitmap , int size)
+void combine_bitmap(char *dst_bitmap , char *src_bitmap , int size , int type)
 {
-    int i=0;
-    
+    int i=0,j=0;
+    unsigned int *ptr=NULL;
+    unsigned int tmp=0;
+    printf("combine_bitmap \n");
+    if( type==1 ) //move up /  shift left
+    {
+       // j=(size-1)/4;
+        # if 0
+        for( i=1;i<64;i=i+4)
+        {
+            ptr=&src_bitmap[i];
+            printf("bef shift right %x \n" ,(unsigned int)*ptr);
+            printf("SRC shift right %x %x %x %x \n" ,(unsigned char)src_bitmap[i],(unsigned char)src_bitmap[i+1],(unsigned char)src_bitmap[i+2],(unsigned char)src_bitmap[i+3]);
+            tmp =(unsigned int)*ptr << 1;
+             printf("after shift right %x \n" ,tmp);
+            *ptr=tmp;
+        }
+        #endif        
+        
+    }    
+    else if( type==2 ) //move down shift right
+    {
+        //j=(size-1)/4;
+        #if 0
+        for( i=1;i<size;i=i+4)
+        {
+            ptr=&src_bitmap[i];
+            // printf("bef shift right %d \n" ,*ptr);
+            tmp =*ptr >> 1;
+            *ptr=tmp;
+            // printf("aft shift right %d \n" ,*ptr);
+        }
+            #endif
+    }        
     for( i=0 ; i< size ; ++i)
     {
         dst_bitmap[i]=dst_bitmap[i] | src_bitmap[i];
     }    
 }
 
-void get_bitmap(int type, char *bitmap , short *unicode)
-{
-    char *tmp_bitmap=(char *)malloc(137);
-    get_data_from_resource(type ,tmp_bitmap,*unicode);
-    memcpy(bitmap, tmp_bitmap ,137);
-    printf(" get unicode %x %x\n", *unicode,*(unicode+1));
-    offset++;
-    /*
-    if(*(unicode+1) >= 0x0e38 && *(unicode+1) <= 0x0e39) //shoe
-    {
-        
-    }
-    else if
-    {
-        
-    }*/
-    if(*(unicode+1) != 0x0e48 || *(unicode+1) == 0x0000 )
-        return ;
-    else
-    {
-        get_bitmap(type,tmp_bitmap,unicode+1);
-        combine_bitmap(bitmap,tmp_bitmap,137);
-    }    
-    // show_font(bitmap,type);
-}
 
 void show_font(char *bitmap , int type)
 {
@@ -220,8 +227,10 @@ void show_font(char *bitmap , int type)
 		font_width = 4;
 		printf("You select L font size \n");        
     }
+    else
+        printf("unknow \n"); 
     
-    for(i=1; i<font_size/2; i++)
+    for(i=1; i<(font_size); i++)
     {
         for(j=7; j>-1; j--)
         {
@@ -238,11 +247,58 @@ void show_font(char *bitmap , int type)
     
 }
 
-char* show_string[10][20]={
+
+void get_bitmap(int type, char *bitmap , short *unicode)
+{
+    char *tmp_bitmap=(char *)malloc(137);
+    get_data_from_resource(type ,tmp_bitmap,*unicode);
+    memcpy(bitmap, tmp_bitmap ,137);
+    printf(" get unicode %x %x\n", *unicode,*(unicode+1));
+    offset++;
+    // show_font(bitmap,type);
+    // memset(bitmap, 0, sizeof(tmp_bitmap));
+    // combine_bitmap(bitmap,tmp_bitmap,137,1);
+    // show_font(bitmap,type);
+    // memset(bitmap, 0, sizeof(tmp_bitmap));
+    // combine_bitmap(bitmap,tmp_bitmap,137,2);    
+    // show_font(bitmap,type);   
+    #if 1
+    if(*(unicode+1) >= 0x0e38 && *(unicode+1) <= 0x0e39) //shoe
     {
-        {"\x44\x0e\x02\x0e\x48\x0e\x44\x0e\x01\x0e\x48\x0e\x00\x00"},
-        
-    },    
+        get_bitmap(type,tmp_bitmap,unicode+1);
+        combine_bitmap(bitmap,tmp_bitmap,137,2);
+         printf(" print shoe \n");
+    }
+    else if( (*(unicode+1) >= 0x0e33 && *(unicode+1) <= 0x0e37)  || (*(unicode+1) >= 0x0e4c && *(unicode+1) <= 0x0e4e)
+            || (*(unicode+1) == 0x0e31 ) ||   (*(unicode+1) == 0x0e47 )   //hat
+           ) 
+    {
+        get_bitmap(type,tmp_bitmap,unicode+1);
+        combine_bitmap(bitmap,tmp_bitmap,137,1); 
+        printf(" print hat \n");    
+    }
+    else if(*(unicode+1) >= 0x0e48 && *(unicode+1) <= 0x0e4B) //voice
+    {
+        get_bitmap(type,tmp_bitmap,unicode+1);
+        combine_bitmap(bitmap,tmp_bitmap,137,0);   
+        printf(" print voice \n");        
+    }
+    else
+    {    
+        return ;
+    }    
+    #endif
+}
+
+
+char* show_string[16][32]=
+{
+    //{
+        {"\x01\x0e\x00\x00"},
+        {"\x02\x0e\x2d\x0e\x1a\x0e\x04\x0e\x13\x0e\x38\x0e\x04\x0e\x23\x0e\x31\x0e\x1A\x0e\x00\x00"},
+        {"\x13\x0e\x38\x0e\x00\x00"},
+        {"\x31\x00\x00\x00"},
+   // },    
 };
 
 int main(void)
@@ -292,11 +348,11 @@ int main(void)
 		fprintf(stderr, "Memory error!!\n");
 		return 0;
 	}    
-    printf("strlen %d size %d \n",strlen(show_string),sizeof(show_string));
+    printf("strlen %ld size %ld \n",strlen(show_string[2][0]),sizeof(show_string));
     memset(bitmap, 0, sizeof(bitmap));
     //for(i=0 ;i<strlen(show_string);i=i+2)
-    ptr=show_string[0][0];    
-    printf(" get unicode %x %x\n", *ptr,*(ptr+1));
+    ptr=(short *)show_string[1][0];    
+   // printf(" get unicode %x %x\n", *ptr,*(ptr+1));
 #if 1
     while(*ptr!=0)     
     {
